fc389f82fbca471466e5cebe460c1c75fc6909cb
Naohito-T <naohito.tanaka0523@gmail.com>
2023-08-06 16:20:20 +0900

    :recycle: Fix: comment

--- dist/commenter/src/main.js
+++ dist/commenter/src/main.js
@@ -25,21 +25,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = void 0;
 const core = __importStar(require("@actions/core"));
-const wait_1 = require("./wait");
+/**
+ * @desc main ブランチに今までコミットされたコミットメッセージを付与する
+ * @note デバッグはシークレット `ACTIONS_STEP_DEBUG` をtrueに設定した場合のみ出力される
+ */
 const main = async ({ github, context }) => {
     try {
-        console.log('github ggg', github);
-        console.log('context ggg', context);
-        const msString = core.getInput('milliseconds') || '1';
-        console.log('gitlog', github.log.debug(`Waiting ${msString} milliseconds ...`)); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        const ms = parseInt(msString, 10);
-        if (isNaN(ms)) {
-            throw new Error('milliseconds not a number');
+        // console.log('github ggg', github);
+        // console.log('context ggg', context);
+        // const msString: string = core.getInput('milliseconds') || '1';
+        // console.log(inspect('gitlog', github.log.debug(`Waiting ${msString} milliseconds ...`)));
+        // const ms: number = parseInt(msString, 10);
+        // if (isNaN(ms)) {
+        //   throw new Error('milliseconds not a number');
+        // }
+        // console.log('gitlog2', github.log.debug(new Date().toTimeString()));
+        // await wait(ms);
+        // console.log('gitlog3', github.log.debug(new Date().toTimeString()));
+        // console.log(core.setOutput('timesssssssssssss', new Date().toTimeString()));
+        // プルリクエストの情報を取得
+        const prNumber = context.payload.pull_request?.number;
+        if (!prNumber) {
+            throw new Error('Pull request number not found.');
         }
-        console.log('gitlog2', github.log.debug(new Date().toTimeString()));
-        await (0, wait_1.wait)(ms);
-        console.log('gitlog3', github.log.debug(new Date().toTimeString()));
-        console.log(core.setOutput('timesssssssssssss', new Date().toTimeString()));
+        // プルリクエストの情報を取得してマージメッセージを取得
+        const pr = await github.rest.pulls.get({
+            ...context.repo,
+            pull_number: prNumber,
+        });
+        const mergeMessage = pr.data.title; // 例としてプルリクエストのタイトルをマージメッセージとする
+        // プルリクエストにマージメッセージを反映させる
+        await github.rest.pulls.update({
+            ...context.repo,
+            pull_number: prNumber,
+            body: mergeMessage,
+        });
+        console.log(`Merge message "${mergeMessage}" has been applied to the pull request.`);
     }
     catch (e) {
         if (e instanceof Error)
--- packages/commenter/src/main.ts
+++ packages/commenter/src/main.ts
@@ -2,30 +2,56 @@ import * as core from '@actions/core';
 import { context } from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
 import { wait } from './wait';
+import { inspect } from 'util';
 
 type GitHubContext = {
   github: InstanceType<typeof GitHub>;
   context: typeof context;
 };
 
+/**
+ * @desc main ブランチに今までコミットされたコミットメッセージを付与する
+ * @note デバッグはシークレット `ACTIONS_STEP_DEBUG` をtrueに設定した場合のみ出力される
+ */
 export const main = async ({ github, context }: GitHubContext): Promise<void> => {
   try {
-    console.log('github ggg', github);
-    console.log('context ggg', context);
-    const msString: string = core.getInput('milliseconds') || '1';
-    console.log('gitlog', github.log.debug(`Waiting ${msString} milliseconds ...`)); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // console.log('github ggg', github);
+    // console.log('context ggg', context);
+    // const msString: string = core.getInput('milliseconds') || '1';
+    // console.log(inspect('gitlog', github.log.debug(`Waiting ${msString} milliseconds ...`)));
 
-    const ms: number = parseInt(msString, 10);
+    // const ms: number = parseInt(msString, 10);
 
-    if (isNaN(ms)) {
-      throw new Error('milliseconds not a number');
+    // if (isNaN(ms)) {
+    //   throw new Error('milliseconds not a number');
+    // }
+
+    // console.log('gitlog2', github.log.debug(new Date().toTimeString()));
+    // await wait(ms);
+    // console.log('gitlog3', github.log.debug(new Date().toTimeString()));
+
+    // console.log(core.setOutput('timesssssssssssss', new Date().toTimeString()));
+    // プルリクエストの情報を取得
+    const prNumber = context.payload.pull_request?.number;
+    if (!prNumber) {
+      throw new Error('Pull request number not found.');
     }
 
-    console.log('gitlog2', github.log.debug(new Date().toTimeString()));
-    await wait(ms);
-    console.log('gitlog3', github.log.debug(new Date().toTimeString()));
+    // プルリクエストの情報を取得してマージメッセージを取得
+    const pr = await github.rest.pulls.get({
+      ...context.repo,
+      pull_number: prNumber,
+    });
+    const mergeMessage = pr.data.title; // 例としてプルリクエストのタイトルをマージメッセージとする
 
-    console.log(core.setOutput('timesssssssssssss', new Date().toTimeString()));
+    // プルリクエストにマージメッセージを反映させる
+    await github.rest.pulls.update({
+      ...context.repo,
+      pull_number: prNumber,
+      body: mergeMessage,
+    });
+
+    console.log(`Merge message "${mergeMessage}" has been applied to the pull request.`);
   } catch (e: unknown) {
     if (e instanceof Error) core.setFailed(e.message);
   }
