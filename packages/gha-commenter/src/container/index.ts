fd083c8d0198a8a546b671b6cb5f1e3657bf7f2a
Naohito-T <naohito.tanaka0523@gmail.com>
2025-03-01 17:31:48 +0900

    ♻️ Update: build

--- packages/gha-commenter/src/container/index.ts
+++ packages/gha-commenter/src/container/index.ts
@@ -1,18 +1,1 @@
-import { GithubRepository } from '../repository';
-import { ChangeSetCommenterUseCase } from '../usecases';
-import {
-  GitHubContext,
-  OctokitBranchCore,
-  OctokitPullRequestCore,
-  OctokitCommitCore,
-} from 'gha-core';
-
-export const container = (octokit: GitHubContext['octokit'], context: GitHubContext['context']) =>
-  new ChangeSetCommenterUseCase(
-    context,
-    new GithubRepository(
-      new OctokitBranchCore(octokit, context),
-      new OctokitPullRequestCore(octokit, context),
-      new OctokitCommitCore(octokit, context),
-    ),
-  );
+export * from './api.container';
